ab620e6d768ededfa46816027c563ae68e07e1b6
PabloJoelMR <pabloreufer@gmail.com>
2024-01-16 10:46:16 +0100

    first commit

---
+++ hello-world/hello.js
@@ -0,0 +1,18 @@
+
+
+module.exports.handler = async (event) => {
+    console.log('Event: ', event);
+    let responseMessage = `${event.resource}`;
+   
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        source: responseMessage,
+      }),
+    }
+  }
+  
+  
